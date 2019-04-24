efd8b99ee06f3f0c8984b8d0449b315a06d6da15
Atuan O <crusifixsso@gmail.com>
2019-04-24 16:48:43 +0100

    Add files via upload

---
+++ C# Test cc Oliver J from answer Digital 2/src/TechTest/Repositories/Models/PersonUpdate.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace TechTest.Repositories.Models 
+{
+    public class PersonUpdate 
+    {
+        public bool Authorised { get; set; }
+        
+        public bool Enabled { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public IEnumerable<Colour> Colours { get; set; }
+    }
+}
